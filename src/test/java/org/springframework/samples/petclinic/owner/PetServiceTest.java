54ccb914d7979e3cf07fcc6879ae4f218371edb5
pouramini.majid <pouramini.majid@gmail.com>
2021-10-15 15:52:25 +0330

    Parametrized test find pet method in pet service

--- src/test/java/org/springframework/samples/petclinic/owner/PetServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetServiceTest.java
@@ -1,11 +1,71 @@
 package org.springframework.samples.petclinic.owner;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.samples.petclinic.utility.SimpleDI;
+import org.junit.After;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.springframework.samples.petclinic.utility.PetTimedCache;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-class PetServiceTest {
+import java.util.*;
 
+import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
+public class PetServiceTest {
+
+	private final Integer petId;
+	private PetService petService;
+
+	public PetServiceTest(Integer petId) {
+		this.petId = petId;
+	}
+
+	private static class MockPetTimedCache extends PetTimedCache {
+		private final Map<Integer, Pet> pets = new HashMap<>();
+
+		public MockPetTimedCache() {
+			super(null);
+			for (int i = 0; i < 10; i++) {
+				Pet pet = new Pet();
+				pet.setName("pet num: " + i);
+				pet.setId(i);
+				pets.put(i, pet);
+			}
+		}
+
+		@Override
+		public Pet get(Integer petId) {
+			return pets.get(petId);
+		}
+	}
+
+	@Before
+	public void setup() {
+		petService = new PetService(
+			new MockPetTimedCache(),
+			null,
+			Mockito.mock(Logger.class)
+		);
+	}
+
+	@Parameters
+	public static List<Integer> petIds() {
+		return Arrays.asList(1, 3, 5 ,9);
+	}
+
+	@Test
+	public void findPetTest() {
+		Pet pet = petService.findPet(petId);
+		assertEquals(pet.getId(), petId);
+	}
+
+	@After
+	public void teardown() {
+		petService = null;
+	}
 }
