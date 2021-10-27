dfca5f4bb2345e30802fc7331a7bd0bf51198ded
Sepehr Rafiee <sepehr.rafiee@snapp.cab>
2021-10-27 17:12:56 +0330

    feat(test): add state verification test for owner

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerTest.java
@@ -2,6 +2,36 @@ package org.springframework.samples.petclinic.owner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class OwnerTest {
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.samples.petclinic.utility.DummyPetStore;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+public class OwnerTest {
+	private static Owner owner;
+
+	@Before
+	public void setup() {
+		owner = new Owner();
+	}
+
+    // State Verification test for Owner
+	@Test
+	public void addPetTestStateVerification() {
+        //given
+		Pet newPet = DummyPetStore.getNewPet();
+
+        //when
+		owner.addPet(newPet);
+
+        //then
+		assertEquals(newPet.getOwner(), owner);
+		assertTrue(owner.getPets().contains(newPet));
+	}
+
+
+    
 }
