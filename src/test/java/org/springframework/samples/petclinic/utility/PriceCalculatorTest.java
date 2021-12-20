6bf54fd1462c31b626a97201dcaf5d3ad3631948
pouramini.majid <pouramini.majid@gmail.com>
2021-12-20 21:48:47 +0330

    test(PriceCalculatorTest): add some test

--- src/test/java/org/springframework/samples/petclinic/utility/PriceCalculatorTest.java
+++ src/test/java/org/springframework/samples/petclinic/utility/PriceCalculatorTest.java
@@ -50,13 +50,75 @@ public class PriceCalculatorTest {
 		when(infantPet.getVisitsUntilAge(INFANT_AGE)).thenReturn(Collections.emptyList());
 	}
 
-	/**
-	 * sample
-	 * */
+	/** This test is for situation that no pet is provided for method */
 	@Test
 	public void calcPriceReturnZeroOnNoPets() {
 		double expectedPrice = 0;
-		double actualPrice = calcPrice(new ArrayList<>(), 0, 0 );
+		double actualPrice = calcPrice(new ArrayList<>(), BASE_CHARGE, BASE_PRICE_PER_PET);
+		assertEquals(expectedPrice, actualPrice);
+	}
+
+
+	@Test
+	public void calcPriceReturnOnePetPriceEvenIfItHasVisits() {
+		ArrayList<Visit> visits = new ArrayList<>();
+		visits.add(newVisit);
+		when(pet.getVisits()).thenReturn(visits);
+
+		ArrayList<Pet> pets = new ArrayList<>();
+		pets.add(pet);
+
+		double expectedPrice = BASE_PRICE_PER_PET * BASE_RARE_COEF;
+		double actualPrice = calcPrice(pets, BASE_CHARGE, BASE_PRICE_PER_PET);
+		assertEquals(expectedPrice, actualPrice);
+	}
+
+	/**
+	 * This test is for when there is only one none-infant pet is provided
+	 * so only base rare coef should be applied
+	 * */
+	@Test
+	public void calcPriceReturnOnePetPriceForSingleNoneInfantPet() {
+		ArrayList<Pet> pets = new ArrayList<>();
+		pets.add(pet);
+
+		double expectedPrice = BASE_PRICE_PER_PET * BASE_RARE_COEF;
+		double actualPrice = calcPrice(pets, BASE_CHARGE, BASE_PRICE_PER_PET);
+		assertEquals(expectedPrice, actualPrice);
+	}
+
+	/**
+	 * This test is for when only an infant pet provided and therefore rare
+	 * infancy coef also should be applied
+	 * */
+	@Test
+	public void calcPriceReturnPriceMultipleRareInfancyCoefForSingleInfantPet() {
+		ArrayList<Pet> pets = new ArrayList<>();
+		pets.add(infantPet);
+		double expectedPrice = BASE_PRICE_PER_PET * BASE_RARE_COEF * RARE_INFANCY_COEF;
+		double actualPrice = calcPrice(pets, BASE_CHARGE, BASE_PRICE_PER_PET);
+		assertEquals(expectedPrice, actualPrice);
+	}
+
+	@Test
+	public void calcPriceShouldNotApplyDiscountIfDiscountMinScoreIsNotReached() {
+		List<Pet> pets = new ArrayList<>();
+		for (int i = 0; i < DISCOUNT_MIN_SCORE - 1; ++i)
+			pets.add(pet);
+		double expectedPrice = pets.size() * SINGLE_PET_PRICE;
+		double actualPrice = calcPrice(pets, BASE_CHARGE, BASE_PRICE_PER_PET);
+		assertEquals(expectedPrice, actualPrice);
+	}
+
+	@Test
+	public void calcPriceShouldApplyDiscountPreVisitAfterDiscountMinScoreIsReachedForNewVisits() {
+		when(pet.getVisitsUntilAge(ADULT_AGE)).thenReturn(newVisits);
+		List<Pet> pets = new ArrayList<>();
+		for (int i = 0; i < DISCOUNT_MIN_SCORE; ++i)
+			pets.add(pet);
+		double priceBeforeMinScore = (DISCOUNT_MIN_SCORE - 1) * SINGLE_PET_PRICE;
+		double expectedPrice = priceBeforeMinScore * DISCOUNT_PRE_VISIT + BASE_CHARGE + SINGLE_PET_PRICE;
+		double actualPrice = calcPrice(pets, BASE_CHARGE, BASE_PRICE_PER_PET);
 		assertEquals(expectedPrice, actualPrice);
 	}
 }
